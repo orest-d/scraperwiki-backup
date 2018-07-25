9ad369bbcd60f0568cdca4309bdfad0678e38f70
orest-d <orest3.dubay@gmail.com>
2018-07-25 11:37:17 +0200

    minor fix

--- download_reduced.sh
+++ download_reduced.sh
@@ -1,2 +1,2 @@
 
-../venv/bin/python3 download.py -v --log download_log.txt -t table.csv --size-limit 100000 --filelist download_files.csv --processed processed_reduced.csv
+../venv/bin/python3 download.py -v --log download_log.txt -t table.csv --size-limit 100000 --filelist download_files_reduced.csv --processed processed_reduced.csv
--- scan.sh
+++ scan.sh
@@ -1,2 +1,2 @@
 
-../venv/bin/python3 download.py -v --log scan_log.txt -t table.csv --filelist scan_files.csv --processed processed_scan.csv --ignore-patterns ignore_less_patterns.txt
+../venv/bin/python3 download.py -v --scan --log scan_log.txt -t table.csv --filelist scan_files.csv --processed processed_scan.csv --ignore-patterns ignore_less_patterns.txt
